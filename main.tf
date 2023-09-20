959df7c92f8a939dc13b4794ac173abc3c094475
Ravi Avula <raviavula.gcp@gmail.com>
2023-09-20 07:33:20 +0100

    vpc creation

--- main.tf
+++ main.tf
@@ -38,3 +38,41 @@ resource "google_storage_bucket" "bucket" {
 
 #   labels = "test gcp bucket"
 # }
+resource "random_id" "instance_id" {
+  byte_length = 4
+}
+
+# create VPC
+resource "google_compute_network" "vpc" {
+  name                    = "${lower(var.company)}-${lower(var.app_name)}-${var.environment}-vpc"
+  auto_create_subnetworks = "false"
+  routing_mode            = "GLOBAL"
+}
+
+# create public subnet
+resource "google_compute_subnetwork" "network_subnet" {
+  name          = "${lower(var.company)}-${lower(var.app_name)}-${var.environment}-subnet"
+  ip_cidr_range = var.network-subnet-cidr
+  network       = google_compute_network.vpc.name
+  region        = var.gcp_region
+}
+# Create VM
+resource "google_compute_instance" "vm_instance_public" {
+  name         = "${lower(var.company)}-${lower(var.app_name)}-${var.environment}-vm${random_id.instance_id.hex}"
+  machine_type = "n2-standard-2"
+  zone         = var.gcp_zone
+  hostname     = "${var.app_name}-vm${random_id.instance_id.hex}.frasers.group"
+  tags         = ["rdp","http"]
+
+  boot_disk {
+    initialize_params {
+      image = "windows-cloud/windows-2019"
+    }
+  }
+
+  network_interface {
+    network       = google_compute_network.vpc.name
+    subnetwork    = google_compute_subnetwork.network_subnet.name
+    access_config { }
+  }
+} 
